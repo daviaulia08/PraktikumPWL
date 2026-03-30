b51422a07c5949adcacd1753772f0caa151b63b8
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-03-31 04:00:27 +0700

    JS 11 dan JS 12 : Implementasi Search & Filter pada Table Filament dan Implementasi Toggle Column pada Table Filament

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -2,30 +2,44 @@
 
 namespace App\Filament\Resources\Posts\Tables;
 
-use Filament\Actions\EditAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DeleteAction;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ImageColumn;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Table;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Forms\Components\DatePicker;
+use Filament\Actions\EditAction;
 
 class PostsTable
 {
     public static function configure(Table $table): Table
     {
         return $table
-            ->defaultSort('created_at', 'desc')
             ->columns([
+                
+                TextColumn::make('id')
+                    ->label('ID')
+                    ->toggleable(isToggledHiddenByDefault: true ),
+
                 TextColumn::make('title')
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable()
+                    ->searchable(),
 
                 TextColumn::make('slug')
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable()
+                    ->searchable(),
 
                 TextColumn::make('category.name')
-                    ->label('Category')
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable()
+                    ->searchable(),
 
                 ColorColumn::make('color'),
 
@@ -36,17 +50,36 @@ class PostsTable
                     ->label('Created At')
                     ->dateTime()
                     ->sortable(),
-            ])->defaultSort('created_at', 'acs')
+
+                TextColumn::make('tags')
+                    ->label('Tags')
+                    ->toggleable(isToggledHiddenByDefault: true ),
+
+                IconColumn::make('published')
+                    ->boolean(),
+            ])
+            ->defaultSort('created_at', 'asc')
             ->filters([
-                //
+                Filter::make('created_at')
+                    ->label('Creation Date')
+                    ->schema([
+                        DatePicker::make('created_at')
+                            ->label('Select Date :'),
+                    ])
+                    ->query(function ($query, $data) {
+                        return $query->when(
+                            $data['created_at'],
+                            fn ($query, $date) => $query->whereDate('created_at', $date)
+                        );
+                    }),
+
+                SelectFilter::make('category_id')
+                    ->relationship('category', 'name')
+                    ->label('Category')
+                    ->preload(),
             ])
             ->recordActions([
                 EditAction::make(),
-            ])
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                ]),
             ]);
     }
 }
